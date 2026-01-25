e5f6e9cf95b0b996f1aaa46b13219f6a64d13c74
kqzb5frspg-alt <kqzb5frspg@privaterelay.appleid.com>
2026-01-25 17:55:44 +0100

    pickup sfx

--- Assets/Scripts/grocery_items.gd
+++ Assets/Scripts/grocery_items.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
+@onready var sfx_pickup: AudioStreamPlayer2D = $"../../Player/sfx_pickup"
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerController:
 		GameManager.add_grocery_item(name)
+		sfx_pickup.play()
 		queue_free()
